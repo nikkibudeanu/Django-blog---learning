1ef86596c772889bd835061dd2896d81340c84e1
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-05-06 16:33:58 +0000

    add configurations in admin panel to have extra features: list filter, search field and list display

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import Post
-form django_summernote.admin import SummernoteModelAdmin
+from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-   
+
+    prepopulated_fields= {'slug' : ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     summernote_fields=('content')
 
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
-    'django_summernote'
+    'django_summernote',
     'blog_app',
 ]
 
