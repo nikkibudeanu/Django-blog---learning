5ff69a4386282fa74381c7301cf5b7353f069f8c
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-05-09 17:05:40 +0000

    connect up the url for the home page

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog_app.urls'), name="blog_urls"),
     path('summernote/', include('django_summernote.urls')),
 ]
