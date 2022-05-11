b94716bfe8da5c67c9e0f7020eb389c9fa88d8e8
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-05-11 15:10:20 +0000

    add allauth library and change settings to create account

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog_app.urls'), name="blog_urls"),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
