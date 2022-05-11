0dbe80440f4e3b8e575105b88e2feb767b450287
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-05-11 15:30:28 +0000

    add links in the navbar for registration and login/logout

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog_app.urls'), name="blog_urls"),
     path('summernote/', include('django_summernote.urls')),
-    path('accounts/', include('allauth.urls')),
+    path("accounts/", include("allauth.urls")),
 ]
